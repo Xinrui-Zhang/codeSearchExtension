2b7251ce0ac33e29e1dcdbda1394129cc59e4299
Xinrui-Zhang <xinrui_zhang@foxmail.com>
2021-08-20 13:13:40 +0800

    增加electron桌面程序

---
+++ dataInputApp/src/App.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Description:
+ * @Version: 2.0
+ * @Autor: xrzhang03
+ * @Date: 2021-08-20 11:09:29
+ * @LastEditors: xrzhang03
+ * @LastEditTime: 2021-08-20 11:30:03
+ */
+
+import { hot } from "react-hot-loader";
+import * as React from "react";
+
+const App = () => <div>Hi here !</div>;
+
+export default hot(module)(App);
